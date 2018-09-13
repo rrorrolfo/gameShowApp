a9be89c4da4f7b354ebc75657deccb7728d8c6fa
Rodolfo Perez <rodolfobeste@gmail.com>
2018-09-13 15:18:09 +0300

    Created function for clicking a letter, one function for comparing it with the phrase and one function so that if it matches the the letter is shown

--- js/app.js
+++ js/app.js
@@ -1,31 +1,29 @@
-////////////  VARIABLES
+////////////  VARIABLES  /////////////
 
 const phrases = [
-    "May the Force be with you",
-    "You talkin to me",
-    "I am your father",
-    "Why so serious",
-    "Bond James Bond",
-    "I see dead people",
-    "I'll be back",
-    "To infinity and beyond",
-    "Houston we have a problem",
-    "Here is Johnny",
-    "Say hello to my little friend",
-    "Hasta la vista baby",
-    "My precious",
+    "you talkin to me",
+    "i am your father",
+    "why so serious",
+    "bond James Bond",
+    "i see dead people",
+    "i ll be back",
+    "here is Johnny",
+    "hasta la vista baby",
+    "my precious",
 ];
 
 
 const start_screen = document.querySelector("#overlay");
 const game_start = document.querySelectorAll(".btn__reset")[0];
-const select_phrase = Math.floor(Math.random() * 14);
+const select_phrase = Math.floor(Math.random() * 10);
 const keyboard = document.querySelector("#qwerty");
 const phrase = document.querySelector("#phrase");
 
 let missed = 0;
 
-//////////  FUNCTIONS
+//////////  FUNCTIONS  //////////////
+
+/// selects randonly the phrase when the person clicks the start game button
 
 function getRandomPhraseAsArray(arr) {
     var a =  arr[select_phrase];
@@ -38,14 +36,63 @@ function getRandomPhraseAsArray(arr) {
     return b
 }
 
-///// Start button funciontality
+/// PUTS THE HIDDEN PHRASE ON THE BOARD
+
+function addPhraseToDisplay(arr){
+
+    for (let i = 0; i < arr.length; i += 1) {
+        const letter = document.querySelector("#phrase ul");
+        const li = document.createElement("li");
+        
+        letter.appendChild(li);
+        li.textContent = arr[i];
+
+        if (li.textContent !== " ") {
+            li.className = "letter"
+        }
+    }
+
+
+}
+
+///// STARTS THE GAME
 
 game_start.addEventListener("click", () => {
     start_screen.style.display = "none";
 
-    console.log(getRandomPhraseAsArray(phrases));
+    const a = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(a);
+
 });
 
 
-/// selecting randonly the phrase when the person clicks the botton
+///// CHECKS THE LETTER 
+
+function checkLetter (chosen_letter) {
+    const letters = document.querySelectorAll(".letter");
+    
+
+    for (let i = 0; i < letters.length; i += 1) {
+        
+        if (chosen_letter === letters[i].textContent) {
+            var letterFound = chosen_letter;
+            letters[i].className += "show";        
+        } 
+    }
+}
+
+
+
+keyboard.addEventListener("click", (event) => {
+
+    const a = event.target;
+    const b = a.textContent;
+
+    if (a.tagName == "BUTTON") {
+        a.className = "chosen";
+        a.setAttribute("disabled", true);
+        checkLetter(b);
+    }
+
+});
 
